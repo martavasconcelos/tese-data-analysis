0eb5e28e4ee39e2bb5c6e52eaa6a892efc5c49ff
martavasconcelos <martavasconcelos14@gmail.com>
2019-03-25 00:22:55 +0000

    table built. Distance calculated for all sessions combinations.

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ function getGoal() {
         callbackFromGoal(xhr.response);
     };
 }
+
 function getActionTypes(session) {
     let ul = document.getElementById("list");
     ul.innerHTML = "";
@@ -77,17 +78,74 @@ function getPathIdBySession() {
 function getMostCommon(response) {
     let responseJson = JSON.parse(response);
     console.log("path id by session: ", responseJson.records);
-
+    createTable(responseJson);
     for (let i = 0; i < responseJson.records.length - 1; i++) {
         for (var j = i + 1; j < responseJson.records.length; j++) {
-            if (responseJson.records[i]._fields[0] !== responseJson.records[j]._fields[0]) {
-                console.log(responseJson.records[i]._fields[0]);
-                console.log(responseJson.records[j]._fields[0]);
-                checkLength(responseJson.records[i]._fields[1], responseJson.records[j]._fields[1]);
+
+            let session1 = responseJson.records[i]._fields[0];
+            let seq1 = responseJson.records[i]._fields[1];
+            let session2 = responseJson.records[j]._fields[0];
+            let seq2 = responseJson.records[j]._fields[1];
+
+            if (session1 !== session2) {
+
+                /* processing(responseJson.records[i]._fields[0],
+                     responseJson.records[i]._fields[1],
+                     responseJson.records[j]._fields[0],
+                     responseJson.records[j]._fields[1]); */
+
+                let similarity = getSimilarity(seq1, seq2)
+                let tr = document.getElementById(session2);
+
+                let thSimilarity = document.createElement("th");
+                thSimilarity.appendChild(document.createTextNode(similarity));
+                tr.appendChild(thSimilarity);
+
             }
         }
     }
+    let table = document.getElementById("table");
+
+    //exportTableToCSV(table,"similarity");
 }
+
+function createTable(responseJson) {
+    for (let i = 0; i < responseJson.records.length; i++) {
+        let session = responseJson.records[i]._fields[0];
+
+        let tableFirstRow = document.getElementById("session");
+        let th = document.createElement("th");
+        th.appendChild(document.createTextNode(session));
+        tableFirstRow.appendChild(th);
+
+        let table = document.getElementById("table");
+
+        let tr = document.createElement("tr");
+        let thSession = document.createElement("th");
+        thSession.appendChild(document.createTextNode(session));
+
+        tr.setAttribute("id", session);
+
+        tr.appendChild(thSession);
+        table.appendChild(tr);
+    }
+}
+
+function getSimilarity(seq1, seq2) {
+    let sequences = sameLength(seq1, seq2);
+    console.log("sequences: ", sequences);
+
+    let distance = 0;
+
+    for (let i = 0; i < sequences.seq1.length; i += 1) {
+        if (sequences.seq1[i] !== sequences.seq2[i]) {
+            distance += 1;
+        }
+    }
+
+    return distance;
+}
+
 /*
 function getSimilarity() {
 
@@ -103,33 +161,27 @@ function getSimilarity() {
     };
 }*/
 
-function checkLength(seq1, seq2) {
-    if(seq1.length === seq2.length){
-        console.log("same length seq1: ", seq1);
-        console.log("same length seq2: ", seq2);
-        getSimilarity(seq1, seq2);
-    }
-    else{
-        let seq1Copy = [...seq1];
-        let seq2Copy = [...seq2];
-        let shortest = getShortest(seq1Copy, seq2Copy);
-        let longest = (shortest === seq1Copy ? seq2Copy : seq1Copy);
+function sameLength( seq1, seq2) {
 
-        while (shortest.length !== longest.length){
-            shortest.push(0);
-        }
+      if(seq1.length === seq2.length){
+          return {seq1: seq1, seq2: seq2};
+      }
+      else{
+          let seq1Copy = [...seq1];
+          let seq2Copy = [...seq2];
+          let shortest = getShortest(seq1Copy, seq2Copy);
+          let longest = (shortest === seq1Copy ? seq2Copy : seq1Copy);
 
-        console.log("shortest: ", shortest);
-        console.log("longest: ", longest);
-    }
-
+          while (shortest.length !== longest.length){
+              shortest.push(0);
+          }
+          return {seq1: shortest, seq2: longest};
+      }
 }
 
-function getSimilarity(seq1, seq2){
-    return 1;
-}
 
-function getShortest(seq1, seq2){
+
+function getShortest(seq1, seq2) {
     return seq1.length > seq2.length ? seq2 : seq1;
 }
 
@@ -164,7 +216,6 @@ function callbackFromMostCommon(response) {
 }
 
 
-
 function callbackFromActionTypes(response) {
     let responseJson = JSON.parse(response);
     let numberOfActions = responseJson.records[0]._fields[1].low;
@@ -192,6 +243,7 @@ function printActionTypes(session, numberOfTypes) {
     li.setAttribute("class", "element"); // added line
     ul.appendChild(li);
 }
+
 function printGoal(session) {
     let ul = document.getElementById("list");
     let li = document.createElement("li");
